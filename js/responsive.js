c898595da6337dfa07406134149c57b8213d9511
Gaurav Yadav <gauravrao630@gmail.com>
2018-04-08 12:51:45 +0530

    compresses images and js

--- js/responsive.js
+++ js/responsive.js
@@ -1,7 +1,1 @@
-if ($(window).width() < 600)
-  {
-    $("#home").removeAttr('data-background-video');
-    $("#home").removeAttr('data-background-video-muted');  
-    $("#home").removeAttr('data-background-video-loop');
-    $("#home").attr('data-background-image', 'images/home.jpg');
-  }
+$(window).width()<600&&($("#home").removeAttr("data-background-video"),$("#home").removeAttr("data-background-video-muted"),$("#home").removeAttr("data-background-video-loop"),$("#home").attr("data-background-image","images/home.jpg"));
